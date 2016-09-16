b83715eafe20cbd082597dfe351395f763f70c80
Dan Palmer <dan@danpalmer.me>
2016-09-16 17:50:00 +0200

    Fix name

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import sys
 from setuptools import setup, find_packages
 
 setup(
-    name='imhotep_bandit',
-    version='0.0.1',
+    name='imhotep-bandit',
+    version='0.0.2',
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
     description="An imhotep plugin for eslint",
     license='MIT',
-    url='https://github.com/danpalmer/imhotep_eslint',
+    url='https://github.com/danpalmer/imhotep-bandit',
     packages=find_packages(),
     install_requires=[
         'imhotep>=1.0.1',
