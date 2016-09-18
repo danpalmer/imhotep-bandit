20272e570c66b6e4f4644a429462da17a320b053
Dan Palmer <dan@danpalmer.me>
2016-09-18 12:11:13 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='imhotep_bandit',
-    version='0.0.3',
+    version='0.0.4',
     author="Dan Palmer",
     author_email="dan@danpalmer.me",
     description="An imhotep plugin for eslint",
